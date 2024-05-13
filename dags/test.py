8955779f79eaa274e7e29756b492e05906a25bfe
sabik.garcia <sabik.garcia@cbre.com>
2024-05-13 17:46:49 +0200

    test dag

---
+++ dags/test.py
@@ -0,0 +1,15 @@
+
+
+default_args = {
+    'owner': 'airflow',
+}
+@dag(default_args=default_args, schedule_interval="@daily", tags=['test'])
+def dag_with_taskflow_api():
+    
+    @task()
+    def print_hello():
+        '''this function prints hello'''
+        print('hello')
+    t1 = print_hello()
+dag_with_taskflow_api = dag_with_taskflow_api()
+
