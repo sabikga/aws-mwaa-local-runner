08cd85f47926a6621e0efb4cfdc0028bf0b7afa0
sabik.garcia <sabik.garcia@cbre.com>
2024-05-14 09:55:44 +0200

    test dag - start_date p

--- dags/test.py
+++ dags/test.py
@@ -1,10 +1,11 @@
 from airflow.decorators import dag, task
+import pendulum
 
 default_args = {
     'owner': 'airflow',
 }
 
-@dag(default_args=default_args, schedule_interval="@daily", tags=['test'])
+@dag(default_args=default_args, start_date=pendulum.datetime(2024, 5, 12, tz="UTC"), schedule_interval="@daily", tags=['test'])
 def dag_with_taskflow_api():
     
     @task()
